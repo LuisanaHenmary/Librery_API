8bb63cdb36280c34b9b99004ca7d92610411f236
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-04-21 17:15:20 -0400

    Create get categories  and create category

--- app/queries.py
+++ app/queries.py
@@ -10,3 +10,17 @@ async def create_author(conn, name):
     """
     row = await conn.fetchrow(query, name)
     return dict(row)
+
+
+async def get_categories(conn):
+    rows = await conn.fetch("SELECT * FROM category")
+    return [dict(row) for row in rows]
+
+async def create_category(conn, name):
+    query = """
+    INSERT INTO category (name)
+    VALUES ($1)
+    RETURNING id, name
+    """
+    row = await conn.fetchrow(query, name)
+    return dict(row)
