680b60a8b8c62dff47181f4e27af6846be5ee0a5
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-04-22 16:06:44 -0400

    Added the book  endpoints

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI, Request
+from fastapi import (
+    FastAPI,
+    Request,
+    Body,
+    status
+)
 from app.db import connect_to_db
-from app.queries import get_authors, create_author, get_categories, create_category, get_user_by_name, create_user
+from app.queries import (
+    get_authors,
+    create_author,
+    get_categories,
+    create_category,
+    get_user_by_name,
+    create_user,
+    create_book,
+    get_books
+)
+
 from datetime import timedelta
 from app.auth import hash_password, verify_password, create_access_token
 
 from models.author import Author
 from models.category import Category
 from models.user import UserLogin, UserRegister
+from models.book import Book
 
 from app.utils import Depends, get_current_user, require_admin, HTTPException
 
@@ -31,7 +47,7 @@ async def get_authors_list(request: Request):
     return await get_authors(conn)
 
 @app.post("/authors")
-async def add_book(author: Author, request: Request, user=Depends(require_admin)):
+async def add_author(author: Author, request: Request, user=Depends(require_admin)):
     conn = request.app.state.db
     return await create_author(conn, author.name)
 
@@ -80,3 +96,25 @@ async def login_user(user: UserLogin, request: Request):
 @app.get("/me")
 async def read_profile(user=Depends(get_current_user)):
     return user
+
+@app.get(
+        path="/books",
+        status_code=status.HTTP_200_OK,
+        tags=["Book"]
+        )
+async def get_books_list(request: Request):
+    conn = request.app.state.db
+    return await get_books(conn)
+
+@app.post(
+        path="/books",
+        status_code=status.HTTP_201_CREATED,
+        tags=["Book"]
+    )
+async def add_book(
+    request: Request,
+    book: Book = Body(...),
+    user=Depends(require_admin)
+    ):
+    conn = request.app.state.db
+    return await create_book(conn, book)
