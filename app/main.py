2ad213a4e4a7a8113e6010acb1a6768ec4ac3b72
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-04-22 12:08:09 -0400

    Added register and login endpoints

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, Request
 from app.db import connect_to_db
-from app.queries import get_authors, create_author, get_categories, create_category
+from app.queries import get_authors, create_author, get_categories, create_category, get_user_by_name, create_user
+from datetime import timedelta
+from app.auth import hash_password, verify_password, create_access_token
 
 from models.author import Author
 from models.category import Category
+from models.user import UserLogin, UserRegister
+
+from app.utils import Depends, get_current_user, require_admin, HTTPException
 
 app = FastAPI()
 
@@ -26,7 +31,7 @@ async def get_authors_list(request: Request):
     return await get_authors(conn)
 
 @app.post("/authors")
-async def add_book(author: Author, request: Request):
+async def add_book(author: Author, request: Request, user=Depends(require_admin)):
     conn = request.app.state.db
     return await create_author(conn, author.name)
 
@@ -36,6 +41,42 @@ async def get_categories_list(request: Request):
     return await get_categories(conn)
 
 @app.post("/categories")
-async def add_category(category: Category, request: Request):
+async def add_category(category: Category, request: Request, user=Depends(require_admin)):
     conn = request.app.state.db
     return await create_category(conn, category.name)
+
+@app.post("/register")
+async def register_user(user: UserRegister, request: Request):
+    conn = request.app.state.db
+    existing_user = await get_user_by_name(conn, user.name)
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Usuario ya existe")
+    
+    hashed = hash_password(user.password)
+    return await create_user(
+        conn,
+        user.ci,
+        user.name,
+        user.email,
+        user.phone_number,
+        hashed
+        )
+
+@app.post("/login")
+async def login_user(user: UserLogin, request: Request):
+    conn = request.app.state.db
+    db_user = await get_user_by_name(conn, user.name)
+
+    if not db_user or not verify_password(user.password, db_user["hashed_password"]):
+        raise HTTPException(status_code=401, detail="Credenciales inválidas")
+    
+    token = create_access_token(
+        data={"sub": db_user["name"], "is_admin": db_user["is_admin"]},
+        expires_delta=timedelta(minutes=30)
+    )
+    return {"access_token": token, "token_type": "bearer"}
+
+@app.get("/me")
+async def read_profile(user=Depends(get_current_user)):
+    return user
