e9e5f19a3329d76d8cc023f709426b1c6cf016e6
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-04-22 12:07:34 -0400

    created get user and add user functions

--- app/queries.py
+++ app/queries.py
@@ -24,3 +24,26 @@ async def create_category(conn, name):
     """
     row = await conn.fetchrow(query, name)
     return dict(row)
+
+async def get_user_by_name(conn, name: str):
+    query = "SELECT * FROM users WHERE name = $1"
+    row = await conn.fetchrow(query, name)
+    return dict(row) if row else None
+
+async def create_user(
+        conn,
+        ci: str,
+        name: str,
+        email: str,
+        phone_number,
+        hashed_password: str,
+        is_admin=False
+        ):
+    
+    query = """
+    INSERT INTO users (name, ci, phone_number, email, is_admin, hashed_password)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING id, name, ci, phone_number, email
+    """
+    row = await conn.fetchrow(query, name, ci, phone_number, email, is_admin, hashed_password)
+    return dict(row)
