c1a55abc4c3634ddbf597e509590e99f8d9d3edc
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-04-23 15:06:49 -0400

    Created get loans and create loan

--- app/queries.py
+++ app/queries.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException
 from asyncpg.exceptions import UniqueViolationError
 
+
+# Author functions
 async def get_authors(conn):
 
     rows = await conn.fetch("SELECT * FROM author")
@@ -21,12 +23,9 @@ async def create_author(conn, name):
     except UniqueViolationError:
         raise HTTPException(status_code=400, detail="This author already exists")
 
-    
     return dict(row)
     
-    
-
-
+# Category functions
 async def get_categories(conn):
     rows = await conn.fetch("SELECT * FROM category")
     return [dict(row) for row in rows]
@@ -44,13 +43,12 @@ async def create_category(conn, name):
         """
         row = await conn.fetchrow(query, name)
 
-
     except UniqueViolationError:
         raise HTTPException(status_code=400, detail="This category already exists")
-
     
     return dict(row)
 
+# User functions
 async def get_user_by_username(conn, username: str):
     query = "SELECT * FROM users WHERE username = $1"
     row = await conn.fetchrow(query, username)
@@ -65,7 +63,6 @@ async def create_user(
     hashed_password: str,
     is_admin=False
 ):
-    
     row = None
 
     try:
@@ -84,11 +81,13 @@ async def create_user(
             is_admin, 
             hashed_password
         )
+
     except UniqueViolationError:
         raise HTTPException(status_code=400, detail="This user or email already exists")
 
     return dict(row)
 
+# Book functions
 async def create_book(conn, book):
 
     row = None
@@ -116,4 +115,32 @@ async def create_book(conn, book):
 
 async def get_books(conn):
     rows = await conn.fetch("SELECT * FROM book")
+    return [dict(row) for row in rows]
+
+# Loan functions
+async def create_loan(conn, loan):
+
+    row = None
+
+    try:
+        query = """
+        INSERT INTO loans (initial_date, delivere_date, id_user, id_book)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, initial_date, delivere_date, id_user, id_book, was_delivered_on_time
+        """
+        row = await conn.fetchrow(
+            query,
+            loan.initial_date,
+            loan.delivere_date,
+            loan.id_user,
+            loan.id_book
+        )
+
+    except Exception:
+        pass
+    
+    return dict(row)
+
+async def get_loans(conn):
+    rows = await conn.fetch("SELECT * FROM loans")
     return [dict(row) for row in rows]
